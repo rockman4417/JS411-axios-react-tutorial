ff896b83d3840948edcfc2fcda5566398735e5c1
rockman4417 <67634648+rockman4417@users.noreply.github.com>
2021-01-19 20:16:55 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import BeerCard from './BeerCard'
+
+class App extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      arrayOfBeer: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.punkapi.com/v2/beers')
+      .then( res => {
+        const arrayOfBeer = res.data
+        this.setState({ arrayOfBeer })
+      })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <ol>{this.state.arrayOfBeer.map((beer, index) => {
+            return (
+              <BeerCard key={index} 
+                        name={beer.name} 
+                        image_url={beer.image_url}
+                        first_brewed={beer.first_brewed}
+                        tagline={beer.tagline}
+                        abv={beer.abv}
+                        description={beer.description}/>
+            )
+          })}</ol>
+        </header>
+      </div>
+    );
+  } 
+  
 }
 
 export default App;
---
+++ src/BeerCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const BeerCard = (props) => {
+    return(
+        <li >
+            <img style={{ height: '200px' }} src={props.image_url} alt={''}></img>
+            <h3>
+            {props.name}
+            <span>{props.first_brewed}</span>
+            </h3>
+            <h4>{props.tagline}</h4>
+            <p><span>{props.abv}</span>{props.description}</p>
+
+            <button></button>
+            
+        </li>)
+}
+
+
+// style={{ listStyle: none }}
+
+export default BeerCard
